0ea2faee771f4c14ff30df36bf2d7e2acb12224b
filipebritocosta88 <filipebritocosta88@gmail.com>
2026-03-09 17:33:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let fluxo = JSON.parse(localStorage.getItem("fluxo")) || []
 let metas = JSON.parse(localStorage.getItem("metas")) || []
 let dividas = JSON.parse(localStorage.getItem("dividas")) || []
@@ -27,11 +26,7 @@ function addFluxo(){
 let desc = document.getElementById("desc").value
 let valor = Number(document.getElementById("valor").value)
 
-fluxo.push({
-desc,
-valor,
-date:new Date().toISOString()
-})
+fluxo.push({desc,valor})
 
 save()
 
@@ -41,7 +36,6 @@ save()
 function deleteFluxo(i){
 
 fluxo.splice(i,1)
-
 save()
 
 }
@@ -50,7 +44,6 @@ save()
 function renderFluxo(){
 
 let div = document.getElementById("listaFluxo")
-
 div.innerHTML=""
 
 fluxo.forEach((f,i)=>{
@@ -72,10 +65,11 @@ div.innerHTML+=`
 }
 
 
+
 function addMeta(){
 
-let nome = document.getElementById("metaNome").value
-let valor = Number(document.getElementById("metaValor").value)
+let nome=document.getElementById("metaNome").value
+let valor=Number(document.getElementById("metaValor").value)
 
 metas.push({
 nome,
@@ -90,13 +84,12 @@ save()
 
 function renderMetas(){
 
-let div = document.getElementById("listaMetas")
-
+let div=document.getElementById("listaMetas")
 div.innerHTML=""
 
 metas.forEach((m,i)=>{
 
-let progresso = (m.atual/m.valor)*100
+let progresso=(m.atual/m.valor)*100
 
 div.innerHTML+=`
 
@@ -106,7 +99,7 @@ div.innerHTML+=`
 
 <div class="progress">
 
-<div class="progress-bar" style="width:${progresso}%"></div>
+<div class="bar" style="width:${progresso}%"></div>
 
 </div>
 
@@ -124,16 +117,16 @@ div.innerHTML+=`
 function deleteMeta(i){
 
 metas.splice(i,1)
-
 save()
 
 }
 
 
+
 function addDivida(){
 
-let nome = document.getElementById("dividaNome").value
-let valor = Number(document.getElementById("dividaValor").value)
+let nome=document.getElementById("dividaNome").value
+let valor=Number(document.getElementById("dividaValor").value)
 
 dividas.push({nome,valor})
 
@@ -142,9 +135,10 @@ save()
 }
 
 
+
 function renderDividas(){
 
-let div = document.getElementById("listaDividas")
+let div=document.getElementById("listaDividas")
 
 div.innerHTML=""
 
@@ -157,38 +151,65 @@ div.innerHTML+=`<div class="card">${d.nome} - ${d.valor}</div>`
 }
 
 
+
 function renderDashboard(){
 
-let entradas = fluxo.reduce((a,b)=>a+b.valor,0)
+let entradas=fluxo.reduce((a,b)=>a+b.valor,0)
+let totalDividas=dividas.reduce((a,b)=>a+b.valor,0)
 
-let totalDividas = dividas.reduce((a,b)=>a+b.valor,0)
+document.getElementById("totalEntradas").innerText=entradas
+document.getElementById("totalDividas").innerText=totalDividas
+document.getElementById("saldoLivre").innerText=entradas-totalDividas
 
-document.getElementById("totalEntradas").innerText = entradas
+renderChart()
 
-document.getElementById("totalDividas").innerText = totalDividas
+}
 
-document.getElementById("saldoLivre").innerText = entradas-totalDividas
+
+
+let chart
+
+function renderChart(){
+
+let ctx=document.getElementById("grafico")
+
+if(chart) chart.destroy()
+
+chart=new Chart(ctx,{
+type:"bar",
+data:{
+labels:["Entradas","Dívidas"],
+datasets:[{
+data:[
+fluxo.reduce((a,b)=>a+b.valor,0),
+dividas.reduce((a,b)=>a+b.valor,0)
+]
+}]
+}
+})
 
 }
 
 
-function iaPergunta(tipo){
 
-let chat = document.getElementById("chat")
+function ia(tipo){
 
-let respostas = {
+let chat=document.getElementById("chat")
 
-economizar:"Reduza gastos recorrentes e crie metas mensais",
+let respostas={
 
-investir:"Diversifique entre renda fixa e ações globais",
+economizar:"Crie metas mensais e reduza gastos recorrentes.",
 
-dividas:"Priorize dívidas com juros maiores"
+investir:"Diversifique entre renda fixa e ações globais.",
+
+dividas:"Priorize quitar dívidas com maior juros."
+
+}
+
+chat.innerHTML+=`<div class="card mt-2">${respostas[tipo]}</div>`
 
 }
 
-chat.innerHTML += `<div class="card mt-2">${respostas[tipo]}</div>`
-
-}
 
 
 function render(){
