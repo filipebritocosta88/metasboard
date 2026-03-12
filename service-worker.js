e4b7b0b9fa12703eb7d283afa430f47e9c15f449
filipebritocosta88 <filipebritocosta88@gmail.com>
2026-03-12 16:39:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
+const cacheName = 'metasboard-v1';
+const assets = [
+  '/metasboard/',
+  '/metasboard/index.html',
+  '/metasboard/app.js',
+  '/metasboard/manifest.json'
+];
 
+// Instala o service worker e guarda os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responde com o cache quando estiver offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
